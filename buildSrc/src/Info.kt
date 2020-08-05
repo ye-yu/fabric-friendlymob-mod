02d738fa7700f26067ed9ffc0c08a319be7a5d27
ye-yu <rafolwen98@gmail.com>
2020-08-05 17:06:52 +0800

    Update gui file - 2

---
+++ buildSrc/src/Info.kt
@@ -0,0 +1,7 @@
+object Info {
+    const val modid = "friendly-mob"
+    const val name = "Befriend!"
+    const val description = "More mobs from befriending monsters!"
+    const val version = "0.0.1"
+    const val group = "fp.yeyu.monsterfriend" //  probably not relevant
+}
