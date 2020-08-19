d6b8ae0b8fa156760050a4dbd72ff655f79c1732
ye-yu <rafolwen98@gmail.com>
2020-08-19 23:08:27 +0800

    Fix wizard experience bug

--- src/main/kotlin/fp/yeyu/monsterfriend/mobs/entity/WizardUtil.kt
+++ src/main/kotlin/fp/yeyu/monsterfriend/mobs/entity/WizardUtil.kt
@@ -1,5 +1,7 @@
 package fp.yeyu.monsterfriend.mobs.entity
 
+import fp.yeyu.monsterfriend.BefriendMinecraft
+import fp.yeyu.monsterfriend.mobs.MobRegistry
 import net.minecraft.enchantment.Enchantment
 import net.minecraft.enchantment.EnchantmentHelper
 import net.minecraft.enchantment.EnchantmentLevelEntry
@@ -77,6 +79,76 @@ object WizardUtil {
             Items.LILAC,
             Items.ROSE_BUSH,
             Items.PEONY
+        )
+
+        private val unobtainables = listOf(
+            Items.SPAWNER,
+            Items.BAT_SPAWN_EGG,
+            Items.BEE_SPAWN_EGG,
+            Items.BLAZE_SPAWN_EGG,
+            Items.CAT_SPAWN_EGG,
+            Items.CAVE_SPIDER_SPAWN_EGG,
+            Items.CHICKEN_SPAWN_EGG,
+            Items.COD_SPAWN_EGG,
+            Items.COW_SPAWN_EGG,
+            Items.CREEPER_SPAWN_EGG,
+            Items.DOLPHIN_SPAWN_EGG,
+            Items.DONKEY_SPAWN_EGG,
+            Items.DROWNED_SPAWN_EGG,
+            Items.ELDER_GUARDIAN_SPAWN_EGG,
+            Items.ENDERMAN_SPAWN_EGG,
+            Items.ENDERMITE_SPAWN_EGG,
+            Items.EVOKER_SPAWN_EGG,
+            Items.FOX_SPAWN_EGG,
+            Items.GHAST_SPAWN_EGG,
+            Items.GUARDIAN_SPAWN_EGG,
+            Items.HOGLIN_SPAWN_EGG,
+            Items.HORSE_SPAWN_EGG,
+            Items.HUSK_SPAWN_EGG,
+            Items.LLAMA_SPAWN_EGG,
+            Items.MAGMA_CUBE_SPAWN_EGG,
+            Items.MOOSHROOM_SPAWN_EGG,
+            Items.MULE_SPAWN_EGG,
+            Items.OCELOT_SPAWN_EGG,
+            Items.PANDA_SPAWN_EGG,
+            Items.PARROT_SPAWN_EGG,
+            Items.PHANTOM_SPAWN_EGG,
+            Items.PIG_SPAWN_EGG,
+            Items.PIGLIN_SPAWN_EGG,
+            Items.PILLAGER_SPAWN_EGG,
+            Items.POLAR_BEAR_SPAWN_EGG,
+            Items.PUFFERFISH_SPAWN_EGG,
+            Items.RABBIT_SPAWN_EGG,
+            Items.RAVAGER_SPAWN_EGG,
+            Items.SALMON_SPAWN_EGG,
+            Items.SHEEP_SPAWN_EGG,
+            Items.SHULKER_SPAWN_EGG,
+            Items.SILVERFISH_SPAWN_EGG,
+            Items.SKELETON_SPAWN_EGG,
+            Items.SKELETON_HORSE_SPAWN_EGG,
+            Items.SLIME_SPAWN_EGG,
+            Items.SPIDER_SPAWN_EGG,
+            Items.SQUID_SPAWN_EGG,
+            Items.STRAY_SPAWN_EGG,
+            Items.STRIDER_SPAWN_EGG,
+            Items.TRADER_LLAMA_SPAWN_EGG,
+            Items.TROPICAL_FISH_SPAWN_EGG,
+            Items.TURTLE_SPAWN_EGG,
+            Items.VEX_SPAWN_EGG,
+            Items.VILLAGER_SPAWN_EGG,
+            Items.VINDICATOR_SPAWN_EGG,
+            Items.WANDERING_TRADER_SPAWN_EGG,
+            Items.WITCH_SPAWN_EGG,
+            Items.WITHER_SKELETON_SPAWN_EGG,
+            Items.WOLF_SPAWN_EGG,
+            Items.ZOGLIN_SPAWN_EGG,
+            Items.ZOMBIE_SPAWN_EGG,
+            Items.ZOMBIE_HORSE_SPAWN_EGG,
+            Items.ZOMBIE_VILLAGER_SPAWN_EGG,
+            Items.ZOMBIFIED_PIGLIN_SPAWN_EGG,
+            MobRegistry.vindor.egg,
+            MobRegistry.evione.egg,
+            MobRegistry.wizard.egg
         )
 
         fun createRandomItem(random: Random): ItemStack {
